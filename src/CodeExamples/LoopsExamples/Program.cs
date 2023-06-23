17149826a5d4bd8d755da5c72854da9628113096
Rashedul Islam <rashidkhan71011@gmail.com>
2023-06-23 08:39:57 +0600

    Loops Added

---
+++ src/CodeExamples/LoopsExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] ages = new int[] { 23, 45, 21, 67, 33 };
+
+int sum = 0;
+for (int i = 0; i < 5; i++)
+{
+    sum = sum + ages[i];
+}
+
+
+int num = 100;
+while (num > 0)
+{
+    num = num / 2;
+    Console.WriteLine($"From While : {num}");
+}
+
+int num2 = 100;
+do
+{
+    num2 = num2 / 2;
+    Console.WriteLine($"From Do-While : {num2}");
+}
+while (num2 > 0);
+
+
+foreach(int age in ages)
+{
+    Console.WriteLine(age);
+}
--- src/CodeExamples/MethodExamples/Program.cs
+++ src/CodeExamples/MethodExamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿
 int[] ages = new int[] { 23, 45, 21, 67, 33 };
 
-double average = (ages[0] + ages[1] + ages[2] + ages[3] + ages[4]) / 5;
+//double average = (ages[0] + ages[1] + ages[2] + ages[3] + ages[4]) / 5;
+
